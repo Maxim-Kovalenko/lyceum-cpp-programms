05d702abe9a24c84159ce56b3e0cf6ae91cb2e24
Max <mx.a.kovalenko@gmail.com>
2020-12-12 12:36:03 +0200

    Reforms
    reforms in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,32 @@
 
 using namespace std;
 
+string compare(int num1, int num2, int num3)
+{
+    int Min;
+    if (num1>num2)
+    {
+        Min = min(num2, num3);
+        cout<<num1*num1<<'\n'<<Min*Min;
+    }
+    else
+    {
+        if(num1<num2)
+        {
+            Min = min(num1, num3);
+            cout<<num2*num2<<'\n'<<Min*Min;
+        }
+        else
+        {
+            Min =
+        }
+    }
 
+}
 
 int main()
 {
-    int a, b, nMax, nMin;
-    cin>>a>>b;
-    nMax = max(a,b);
-    nMin = min(a,b);
-    cout<<nMax*nMax<<'\n'<<nMin*nMin;
+    int a, b, c;
+    cin>>a>>b>>c;
+    compare(a, b, c);
 }
-
