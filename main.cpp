8116e7fcba0e1db20e2f7a1dd8dd6889006be966
Max <mx.a.kovalenko@gmail.com>
2020-12-22 09:26:01 +0200

    reforms
    no time to explain!

--- main.cpp
+++ main.cpp
@@ -10,11 +10,13 @@ int main()
 
 string compare(a, b, c)
 {
-    int num1, num2, num3;
+    int num1, num2, num3, num4;
     if(a>b)
     {
         num1 = max(a, c)
-        num2 = max(num1, b)
+        num2 = min(a, c)
+        num3 = max(num1, b)
+        num4 = min()
     }
     else
     {
