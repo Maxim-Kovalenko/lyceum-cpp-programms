f0b90cb551261a6ad7cd1fc3e88f519291c80bd6
Max <mx.a.kovalenko@gmail.com>
2021-01-30 12:30:09 +0200

    new project
    new project!

---
+++ max_ohno/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    double num;
+    cin>>num;
+    if(num==round(num))
+    {
+        int num1 = round(num);
+        int sum_num1 = ;
+        int sum_num2 = ;
+        int sum_num3 = ;
+        int sum_num4 = ;
+        int sum = ;
+        cout<<sum;
+    }
+    else
+    {
+        double num2 = sqrt(abs(num));
+        cout<<num2;
+    }
+}
+
